aefd9544aba5cd2556e9589fc12b3e153dc63e5d
spazdrum <spazdrum@gmail.com>
2020-03-13 21:36:30 -0400

    Created hover event on content-destination

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const destHover = document.querySelector(".content-destination img")
+destHover.addEventListener("mouseenter", () => {
+    destHover.style.transform = "scale(1.2)"
+    destHover.style.transition = "all 0.3s"
+})
+
+destHover.addEventListener("mouseleave", () => {
+    destHover.style.transform = "scale(1)"
+})
